41e8b76370aaa6eeaf91cde7fb8fc4b67fe894fa
Pacman29 <balyakin.d.v@gmail.com>
2017-06-15 19:11:15 +0300

    test selected

--- app/components/change-word.js
+++ app/components/change-word.js
@@ -7,6 +7,7 @@ export default Ember.Component.extend({
   dictionary: Ember.inject.service('dictionary'),
   changeword_controller: Ember.inject.controller('changeword'),
 
+  word: null,
   select_dialects: null,
   select_slangs: null,
 
@@ -14,10 +15,38 @@ export default Ember.Component.extend({
     this._super(...arguments);
     this.get('dictionary').dialects().then(function (dialects) {
       this.set('select_dialects',dialects);
+      this.get('select_dialects').forEach(iter => {
+        if(this.get('word').get('dialect') === iter.get('dialect')){
+          iter.set('selected',true);
+        }
+      })
     }.bind(this));
     this.get('dictionary').slangs().then(function (slangs) {
       this.set('select_slangs',slangs);
+      this.get('select_slangs').forEach(iter => {
+        if(this.get('word').get('slang') === iter.get('slang')){
+          iter.set('selected',true);
+        }
+      })
     }.bind(this));
+  },
+
+  dialect: null,
+  slang: null,
+  description: null,
+
+  actions: {
+    getslang(value){
+      this.set('slang',value);
+    },
+
+    getdialect(value){
+      this.set('dialect',value);
+    },
+
+    getdescription(value){
+      this.set('description',value);
+    }
   },
 
 
--- app/controllers/changeword.js
+++ app/controllers/changeword.js
@@ -6,17 +6,4 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   word: null,
 
-  dialect: null,
-  slang: null,
-
-  actions: {
-    getslang(value){
-      this.set('slang',value);
-    },
-
-    getdialect(value){
-      this.set('dialect',value);
-    },
-  },
-
 });
